6c1a912d20cac15b348ec8b5c70e646476ebe162
Joshua Ani <josh4820@gmail.com>
2020-11-20 23:03:50 -0500

    Chunk voxelization

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use voxelization::material_mesh::MaterialMesh;
 use voxelization::voxels::Voxels;
 
 fn main() {
-    let source = fs::read_to_string("assets/test.obj").expect("File not read");
+    let source = fs::read_to_string("assets/test2.obj").expect("File not read");
     let mesh = MaterialMesh::from_obj_1_material(source).expect("Invalid mesh");
 
     let voxels = Voxels::from(mesh);
+    voxels.export_debug_obj("assets/debug/voxels.obj");
 }
--- src/voxels.rs
+++ src/voxels.rs
@@ -1,11 +1,62 @@
-use cgmath::prelude::*;
-use cgmath::Vector3;
 use fnv::FnvHashMap;
+use tri_mesh::prelude::*;
+use std::path::Path;
 
 use crate::material_mesh::{Axis, MaterialID, MaterialMesh};
 
 pub type Vec3i = Vector3<i32>;
 pub type Vec3f = Vector3<f32>;
+
+#[derive(Debug)]
+struct DebugMeshBuilder {
+    positions: Vec<f64>,
+    indexes: Vec<u32>,
+    materials: Vec<MaterialID>,
+}
+
+impl DebugMeshBuilder {
+    fn new() -> Self {
+        Self {
+            positions: vec![],
+            indexes: vec![],
+            materials: vec![],
+        }
+    }
+
+    /// Add a cube given min coordinates, side length, and material
+    fn add_cube(&mut self, min: Vec3, length: f64, material: MaterialID) {
+        let offset = self.positions.len() / 3;
+        self.indexes.extend(vec![
+            2, 6, 4, 4, 0, 2, 6, 7, 5, 5, 4, 6, 7, 3, 1, 1, 5, 7,
+            3, 2, 0, 0, 1, 3, 0, 4, 5, 5, 1, 0, 2, 3, 7, 7, 6, 2,
+        ].into_iter().map(|i| offset as u32 + i));
+
+        for z in 0..2 {
+            for y in 0..2 {
+                for x in 0..2 {
+                    self.positions.extend(vec![
+                        min.x + length * x as f64,
+                        min.y + length * y as f64,
+                        min.z + length * z as f64,
+                    ]);
+                }
+            }
+        }
+
+        self.materials.extend(vec![material; 12]);
+    }
+
+    fn build(self) -> MaterialMesh {
+        MaterialMesh::new(
+            MeshBuilder::new()
+                .with_positions(self.positions)
+                .with_indices(self.indexes)
+                .with_tags(self.materials)
+                .build()
+                .expect("Invalid mesh")
+        )
+    }
+}
 
 /// The voxelization. Includes chunks, pure voxels, and complex voxels.
 #[derive(Clone, Debug, Default)]
@@ -17,16 +68,81 @@ impl Voxels {
     fn new() -> Self {
         Self::default()
     }
+
+    /// Export this voxelization as an obj for debugging
+    pub fn export_debug_obj<P: AsRef<Path>>(&self, path: P) {
+        let mut builder = DebugMeshBuilder::new();
+        
+        for (pos, chunk) in &self.chunks {
+            match chunk {
+                Chunk::Uniform(material) =>
+                    builder.add_cube(pos.cast().unwrap() * Chunk::SIZE as f64, Chunk::SIZE as f64, *material)
+            }
+        }
+
+        builder.build().export_debug_obj(path)
+    }
+
+    fn fill_uniform_chunks(&mut self, ranges_yz: Vec<(f64, f64, Vec<(f64, f64, i32)>)>) {
+        for (y, z, ranges) in &ranges_yz {
+            let chunk_y = *y as i32 / Chunk::SIZE as i32;
+            let chunk_z = *z as i32 / Chunk::SIZE as i32;
+            let mut inside = 0;
+            let mut start = 0;
+
+            for (min, max, grad) in ranges {
+                if inside != 0 {
+                    let end = (*min / Chunk::SIZE as f64).floor() as i32;
+
+                    for chunk_x in start..end {
+                        self.chunks.insert(
+                            vec3(chunk_x, chunk_y, chunk_z),
+                            // Placeholder until multimaterial works
+                            Chunk::Uniform(MaterialID::new(1)),
+                        );
+                    }
+                }
+
+                inside = (inside + grad).rem_euclid(2);
+                start = (*max / Chunk::SIZE as f64).ceil() as i32;
+            }
+        }
+    }
 }
 
 impl From<MaterialMesh> for Voxels {
     fn from(mesh: MaterialMesh) -> Self {
-        let (slices, min_z) = mesh.axis_slice(Axis::Z, Chunk::SIZE as f64);
+        let slices = mesh.axis_slice(Axis::Z, Chunk::SIZE as f64);
+        let slices = slices
+            .into_iter()
+            .flat_map(|(z, slice)| {
+                slice
+                    .axis_slice(Axis::Y, Chunk::SIZE as f64)
+                    .into_iter()
+                    .map(move |(y, slice)| (y, z, slice))
+            })
+            .collect::<Vec<_>>();
 
-        for (i, slice) in slices.iter().enumerate() {
-            slice.export_debug_obj(format!("assets/debug/slice_{:03}.obj", i));
-        }
-        Self::default()
+        // Obtain ranges as a map from (y, z) coords to a vector of (min, max, in-out gradient) tuples
+        let ranges_yz = slices
+            .into_iter()
+            .map(|(y, z, slice)| {
+                (
+                    y,
+                    z,
+                    slice.axis_ranges_and_in_out_gradients(
+                        Axis::X,
+                        (Chunk::SIZE * Chunk::SIZE) as f64,
+                    ),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        let mut voxels = Self::new();
+
+        voxels.fill_uniform_chunks(ranges_yz);
+
+        voxels
     }
 }
 
