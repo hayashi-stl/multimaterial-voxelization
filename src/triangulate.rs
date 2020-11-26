063c6ff3e0cf606f43d1a99aacd53b26bd4c7c29
Joshua Ani <josh4820@gmail.com>
2020-11-26 00:23:43 -0500

    Started cycle extraction

--- src/triangulate.rs
+++ src/triangulate.rs
@@ -5,6 +5,7 @@
 use float_ord::FloatOrd;
 use fnv::{FnvHashMap, FnvHashSet};
 use petgraph::prelude::*;
+use std::collections::VecDeque;
 use tri_mesh::prelude::*;
 
 use crate::util::{GraphEx, HashVec2, Vec2};
@@ -19,6 +20,71 @@ pub struct Polygon {
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum TriangulateError {
     UnbalancedDegreeVertex,
+}
+
+/// Used for the plane sweep.
+/// Contains next vertex and
+/// neighboring vertices of split lines in clockwise order.
+#[derive(Debug)]
+struct SweepVertex {
+    target: NodeIndex,
+    kind: SweepKind,
+}
+
+#[derive(Debug)]
+enum SweepKind {
+    Start(Option<NodeIndex>),
+    End(Option<NodeIndex>),
+    Continue([Option<NodeIndex>; 2], bool),
+    Split([Option<NodeIndex>; 3]),
+    Merge([Option<NodeIndex>; 3]),
+}
+
+impl SweepVertex {
+    fn new(target: NodeIndex, kind: SweepKind) -> Self {
+        Self { target, kind }
+    }
+
+    /// Adds the other node of a split line to the right of this node
+    fn add_right(&mut self, node: NodeIndex, up: bool) {
+        match &mut self.kind {
+            SweepKind::Start(n) => *n = Some(node),
+            SweepKind::End(_) => unreachable!(),
+            SweepKind::Continue([a, b], flip) => *if *flip { a } else { b } = Some(node),
+            SweepKind::Merge([_, n, _]) => *n = Some(node),
+            // More complicated because we need to figure out which side (bottom or top)
+            // the split line is on
+            SweepKind::Split([b, _, t]) => *if up { t } else { b } = Some(node),
+        }
+    }
+
+    /// Adds the other node of a split line to the left of this node
+    fn add_left(&mut self, node: NodeIndex, up: bool) {
+        match &mut self.kind {
+            SweepKind::Start(_) => unreachable!(),
+            SweepKind::End(n) => *n = Some(node),
+            SweepKind::Continue([a, b], flip) => *if *flip { b } else { a } = Some(node),
+            SweepKind::Split([_, n, _]) => *n = Some(node),
+
+            // More complicated because we need to figure out which side (bottom or top)
+            // the split line is on
+            SweepKind::Merge([t, _, b]) => *if up { b } else { t } = Some(node),
+        }
+    }
+
+    fn targets(self) -> Vec<NodeIndex> {
+        match self.kind {
+            SweepKind::Start(n) | SweepKind::End(n) => vec![n],
+
+            SweepKind::Continue([a, b], _) => vec![a, b],
+
+            SweepKind::Split([a, b, c]) | SweepKind::Merge([a, b, c]) => vec![a, b, c],
+        }
+        .into_iter()
+        .flatten()
+        .chain(std::iter::once(self.target))
+        .collect()
+    }
 }
 
 impl Polygon {
@@ -102,14 +168,60 @@ impl Polygon {
     pub fn from_boundary(mut boundary: Graph<Vec2, ()>) -> Result<Self, TriangulateError> {
         Self::fix_bad_degrees(&mut boundary)?;
         // Now every vertex is indegree-1 outdegree-1.
-        println!("graph: {:?}", boundary);
+        //println!("graph: {:?}", boundary);
 
         Ok(Self { boundary })
     }
 
+    fn monotone_cycles(sweep: FnvHashMap<NodeIndex, SweepVertex>) -> Vec<Vec<NodeIndex>> {
+        let mut target_map = sweep
+            .into_iter()
+            .map(|(k, v)| (k, v.targets()))
+            .collect::<FnvHashMap<_, _>>();
+
+        target_map
+            .iter()
+            .for_each(|(k, v)| println!("node: {:?}, targets: {:?}", k, v));
+
+        let mut cycles = vec![];
+
+        // Extract cycles
+        while let Some(start) = target_map.keys().next().copied() {
+            let mut node = start;
+            let mut prev = node;
+            let mut cycle = vec![];
+
+            while {
+                cycle.push(node);
+                let targets = target_map.get_mut(&node).unwrap();
+
+                let new_node = if prev == node {
+                    targets.pop().unwrap()
+                } else {
+                    todo!()
+                };
+
+                if targets.is_empty() {
+                    target_map.remove(&node);
+                }
+
+                prev = node;
+                node = new_node;
+                node != start
+            } {}
+
+            cycles.push(cycle);
+        }
+
+        cycles
+    }
+
     /// Decomposes the polygon into x-monotone polygons
     /// (actually monotone mountains, where one side is just a line segment)
-    fn monotone_decompose(&mut self) {
+    /// Returns a list of monotone mountains, where
+    /// each item contains the vertices of the monotone mountain
+    /// in circular order.
+    fn monotone_decompose(&mut self) -> Vec<Vec<NodeIndex>> {
         let boundary = &mut self.boundary;
 
         // Plane sweep from left to right
@@ -150,22 +262,23 @@ impl Polygon {
         // >= its left coordinate and < the sweep line,
         // called the "helper" in https://people.csail.mit.edu/indyk/6.838-old/handouts/lec4.pdf
         let mut edges: Vec<(NodeIndex, NodeIndex, Option<NodeIndex>)> = vec![];
+        let mut sweep_nodes = FnvHashMap::default();
 
         for (i, node) in nodes.into_iter().enumerate() {
             // Nodes exist because each node has an incoming edge and an outgoing edge.
             let mut node_a = boundary.edges_in(node).next().unwrap().source();
             let mut node_b = boundary.edges(node).next().unwrap().target();
+            let out_node = node_b;
             let index_a = index_map[&node_a];
             let index_b = index_map[&node_b];
 
             let pos = boundary[node];
 
+            // Edge to cut. Left node first.
+            let mut cut = None;
+
             if index_a > i && index_b > i {
                 // Split/start vertex
-                println!("Split/start!");
-                println!("{}: {:?}", i, node);
-                println!("edges: {:?}", edges);
-                println!("before swap: a: {:?}, b: {:?}", node_a, node_b);
                 if ((boundary[node_a] - pos).dot(boundary[node_b] - pos) < 0.0
                     && boundary[node_a].y > boundary[node_b].y)
                     || (boundary[node_a] - pos).perp_dot(boundary[node_b] - pos) < 0.0
@@ -183,9 +296,6 @@ impl Polygon {
                         FloatOrd(l_pos.y + (r_pos.y - l_pos.y) * t)
                     })
                 {
-                    println!("after swap: a: {:?}, b: {:?}", node_a, node_b);
-                    println!("index: {}", index);
-                    println!();
                     // Add node as helper in the case of a start vertex
                     // to handle an edge case dealing with a split vertex
                     let helper = if index % 2 == 0 { Some(node) } else { None };
@@ -194,27 +304,32 @@ impl Polygon {
 
                     if index % 2 != 0 {
                         // Split vertex
+                        sweep_nodes.insert(
+                            node,
+                            SweepVertex::new(out_node, SweepKind::Split([None; 3])),
+                        );
+
                         // Look at edges above and below and add a diagonal to the one
                         // with the rightmost left helper coordinate.
-                        let helper = vec![edges[index - 1].2, edges[index + 2].2]
+                        let (h_index, helper) = vec![edges[index - 1].2, edges[index + 2].2]
                             .into_iter()
-                            .flatten()
-                            .max_by_key(|n| FloatOrd(boundary[*n].x))
+                            .enumerate()
+                            .flat_map(|(i, n)| n.map(|n| (i, n)))
+                            .max_by_key(|(_, n)| FloatOrd(boundary[*n].x))
                             .unwrap();
-                        boundary.add_edge(node, helper, ());
-                        boundary.add_edge(helper, node, ());
+                        cut = Some((helper, node, h_index == 0));
 
                         // Update helpers
                         edges[index - 1].2 = Some(node);
                         edges[index + 2].2 = Some(node);
+                    } else {
+                        // Start vertex
+                        sweep_nodes
+                            .insert(node, SweepVertex::new(out_node, SweepKind::Start(None)));
                     }
                 }
             } else if index_a < i && index_b < i {
                 // Merge/end vertex
-                println!("Merge/end!");
-                println!("{}: {:?}", i, node);
-                println!("edges: {:?}", edges);
-                println!("before swap: a: {:?}, b: {:?}", node_a, node_b);
                 if ((boundary[node_a] - pos).dot(boundary[node_b] - pos) < 0.0
                     && boundary[node_a].y > boundary[node_b].y)
                     || (boundary[node_a] - pos).perp_dot(boundary[node_b] - pos) > 0.0
@@ -226,17 +341,20 @@ impl Polygon {
                     .iter()
                     .position(|(s, t, _)| (*s, *t) == (node_a, node))
                     .unwrap();
-                println!("after swap: a: {:?}, b: {:?}", node_a, node_b);
-                println!("index: {}", index);
-                println!();
 
                 if index % 2 != 0 {
                     // Merge vertex
-                    for (_, _, helper) in vec![edges[index], edges[index + 1]] {
+                    sweep_nodes.insert(
+                        node,
+                        SweepVertex::new(out_node, SweepKind::Merge([None; 3])),
+                    );
+
+                    for (i, (_, _, helper)) in
+                        vec![edges[index], edges[index + 1]].into_iter().enumerate()
+                    {
                         if let Some(helper) = helper {
                             if helper != node_a && helper != node_b {
-                                boundary.add_edge(node, helper, ());
-                                boundary.add_edge(helper, node, ());
+                                cut = Some((helper, node, i == 0));
                             }
                         }
                     }
@@ -246,14 +364,16 @@ impl Polygon {
                     edges[index + 2].2 = Some(node);
                 } else {
                     // End vertex
+                    sweep_nodes.insert(node, SweepVertex::new(out_node, SweepKind::End(None)));
+
                     if let Some(helper) = vec![edges[index], edges[index + 1]]
                         .into_iter()
                         .max_by_key(|(s, _, _)| index_map[s])
                         .unwrap()
                         .2
                     {
-                        boundary.add_edge(node, helper, ());
-                        boundary.add_edge(helper, node, ());
+                        // Don't care about up/down
+                        cut = Some((helper, node, false));
                     }
                 }
 
@@ -261,41 +381,53 @@ impl Polygon {
                 edges.remove(index);
             } else {
                 // Vertex crosses sweep line
-                println!("Continuation!");
-                println!("{}: {:?}", i, node);
-                println!("edges: {:?}", edges);
-                println!("before swap: a: {:?}, b: {:?}", node_a, node_b);
+                let mut flipped = false;
                 if index_a > i {
                     std::mem::swap(&mut node_a, &mut node_b);
+                    flipped = true
                 }
+
+                sweep_nodes.insert(
+                    node,
+                    SweepVertex::new(out_node, SweepKind::Continue([None; 2], flipped)),
+                );
 
                 let index = edges
                     .iter()
                     .position(|(s, t, _)| (*s, *t) == (node_a, node))
                     .unwrap();
-                println!("after swap: a: {:?}, b: {:?}", node_a, node_b);
-                println!("index: {}", index);
-                println!();
 
                 if let Some(helper) = edges[index].2 {
                     if helper != node_a {
-                        boundary.add_edge(node, helper, ());
-                        boundary.add_edge(helper, node, ());
+                        cut = Some((helper, node, index % 2 == 1));
                     }
                 }
 
                 edges[index] = (node, node_b, None);
                 edges[index ^ 1].2 = Some(node);
             }
+
+            if let Some((cut_l, cut_r, up)) = cut {
+                boundary.add_edge(cut_l, cut_r, ());
+                boundary.add_edge(cut_r, cut_l, ());
+
+                sweep_nodes.get_mut(&cut_l).unwrap().add_right(cut_r, up);
+                sweep_nodes.get_mut(&cut_r).unwrap().add_left(cut_l, up);
+            }
         }
+
+        Self::monotone_cycles(sweep_nodes)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use fnv::FnvHasher;
     use petgraph::algo;
     use petgraph::data::{Element, FromElements};
+    use std::hash::{Hash, Hasher};
+    use std::num::Wrapping;
 
     fn create_graph<N>(vertices: Vec<N>, edges: Vec<(usize, usize)>) -> Graph<N, ()> {
         Graph::from_elements(
@@ -308,6 +440,49 @@ mod test {
                     weight: (),
                 })),
         )
+    }
+
+    fn create_decomposition(cycles: Vec<Vec<usize>>) -> FnvHashSet<Ring<NodeIndex>> {
+        cycles
+            .into_iter()
+            .map(|v| Ring(v.into_iter().map(NodeIndex::new).collect()))
+            .collect()
+    }
+
+    fn testable_decomposition(cycles: Vec<Vec<NodeIndex>>) -> FnvHashSet<Ring<NodeIndex>> {
+        cycles
+            .into_iter()
+            .map(|v| Ring(v.into_iter().collect()))
+            .collect()
+    }
+
+    #[derive(Clone, Debug, Eq)]
+    struct Ring<T: Clone + Eq + Hash>(VecDeque<T>);
+
+    impl<T: Clone + Eq + Hash> PartialEq for Ring<T> {
+        fn eq(&self, other: &Self) -> bool {
+            let mut rotatable = other.0.clone();
+            (0..self.0.len()).into_iter().all(|_| {
+                rotatable.rotate_right(1);
+                rotatable == self.0
+            })
+        }
+    }
+
+    impl<T: Clone + Eq + Hash> Hash for Ring<T> {
+        fn hash<H: Hasher>(&self, state: &mut H) {
+            let int = self
+                .0
+                .iter()
+                .map(|t| {
+                    let mut hasher = FnvHasher::default();
+                    t.hash(&mut hasher);
+                    Wrapping(hasher.finish())
+                })
+                .product::<Wrapping<u64>>();
+
+            int.hash(state)
+        }
     }
 
     #[test]
@@ -468,7 +643,6 @@ mod test {
         ));
     }
 
-
     #[test]
     fn test_monotone_decomposition_triangle() {
         let mut polygon = Polygon::from_boundary(create_graph(
@@ -508,14 +682,16 @@ mod test {
             ],
             vec![(0, 1), (1, 2), (2, 3), (3, 0), (1, 3), (3, 1)],
         );
+        let expected_cycles = create_decomposition(vec![vec![0, 1, 2], vec![3, 2, 1]]);
 
-        polygon.monotone_decompose();
+        let cycles = testable_decomposition(polygon.monotone_decompose());
         assert!(algo::is_isomorphic_matching(
             &polygon.boundary,
             &expected,
             |x, y| x == y,
             |x, y| x == y
         ));
+        assert_eq!(cycles, expected_cycles);
     }
 
     #[test]
@@ -713,22 +889,63 @@ mod test {
         // edge endpoints is not enough to determine which edge comes first.
         let mut polygon = Polygon::from_boundary(create_graph(
             vec![
-                vec2(3.0, 0.0), vec2(7.0, 0.0), vec2(9.0, 5.0), vec2(6.0, 2.0), vec2(7.0, 4.0),
-                vec2(4.0, 4.0), vec2(2.0, 2.0), vec2(5.0, 3.0), vec2(4.0, 1.0), vec2(0.0, 2.0),
+                vec2(3.0, 0.0),
+                vec2(7.0, 0.0),
+                vec2(9.0, 5.0),
+                vec2(6.0, 2.0),
+                vec2(7.0, 4.0),
+                vec2(4.0, 4.0),
+                vec2(2.0, 2.0),
+                vec2(5.0, 3.0),
+                vec2(4.0, 1.0),
+                vec2(0.0, 2.0),
             ],
             vec![
-                (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 0),
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 5),
+                (5, 6),
+                (6, 7),
+                (7, 8),
+                (8, 9),
+                (9, 0),
             ],
         ))
         .unwrap();
         let expected = create_graph(
             vec![
-                vec2(3.0, 0.0), vec2(7.0, 0.0), vec2(9.0, 5.0), vec2(6.0, 2.0), vec2(7.0, 4.0),
-                vec2(4.0, 4.0), vec2(2.0, 2.0), vec2(5.0, 3.0), vec2(4.0, 1.0), vec2(0.0, 2.0),
+                vec2(3.0, 0.0),
+                vec2(7.0, 0.0),
+                vec2(9.0, 5.0),
+                vec2(6.0, 2.0),
+                vec2(7.0, 4.0),
+                vec2(4.0, 4.0),
+                vec2(2.0, 2.0),
+                vec2(5.0, 3.0),
+                vec2(4.0, 1.0),
+                vec2(0.0, 2.0),
             ],
             vec![
-                (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 0),
-                (0, 8), (8, 0), (1, 3), (3, 1), (3, 7), (7, 3), (5, 7), (7, 5)
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 5),
+                (5, 6),
+                (6, 7),
+                (7, 8),
+                (8, 9),
+                (9, 0),
+                (0, 8),
+                (8, 0),
+                (1, 3),
+                (3, 1),
+                (3, 7),
+                (7, 3),
+                (5, 7),
+                (7, 5),
             ],
         );
 
